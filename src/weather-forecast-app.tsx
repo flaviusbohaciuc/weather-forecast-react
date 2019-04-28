d7d8ee5092472ed0d16964921bf55d01c10f4759
Flavius Bohaciuc <flaviusbohaciuc@gmail.com>
2019-04-28 14:34:25 +0100

    clean template and add minimal setup

---
+++ src/weather-forecast-app.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const WeatherForecast = () => {
+  return (
+    <div>
+      <h1>
+        Weather Forecast
+      </h1>
+    </div>
+  );
+};
+
+export default WeatherForecast;
